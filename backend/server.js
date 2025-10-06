d3535ef650d2d55eaa54ac1d47e517f2d8e3beb3
Satyam Rana <satyamranatc@gmail.com>
2025-10-06 15:27:25 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/dbConfig.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+app.use(express.json());
+
+connectDB();
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is live" });
+});
+
+
+app.listen(PORT, () => {
+  console.log('Server running ...')
+});
